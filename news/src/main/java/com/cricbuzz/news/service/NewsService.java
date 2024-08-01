6a463d4a7a26a94b0fff131a4b76215f827ee75b
Jitendra Tiwari <jkjitendra725@gmail.com>
2024-08-01 22:54:44 +0530

    updated createNews method to accept image

--- news/src/main/java/com/cricbuzz/news/service/NewsService.java
+++ news/src/main/java/com/cricbuzz/news/service/NewsService.java
@@ -3,10 +3,11 @@ package com.cricbuzz.news.service;
 import com.cricbuzz.news.dto.NewsRequestDTO;
 import com.cricbuzz.news.dto.NewsResponseDTO;
 import com.cricbuzz.news.dto.PageableResponse;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface NewsService {
 
-    NewsResponseDTO createNews(NewsRequestDTO news);
+    NewsResponseDTO createNews(NewsRequestDTO news, MultipartFile image);
 
     PageableResponse<NewsResponseDTO> getNewsByTag(String tag, int page, int size);
 
